7a3dc45f1304e9465bcf1d8b0477e6877c1010ee
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-01-28 09:45:53 +0530

    add slider

---
+++ src/components/buttons/LogoutButton.jsx
@@ -0,0 +1,16 @@
+import 'daisyui/dist/full.css'
+
+const LogoutButton = ({ children, onClick }) => {
+    return (
+      <div>
+        <button
+          onClick={onClick}
+          className="btn btn-outline btn-secondary w-80"
+        >
+          {children}
+        </button>
+      </div>
+    );
+  };
+  
+  export default LogoutButton;
