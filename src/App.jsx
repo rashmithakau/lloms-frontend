4e7de437107142efc1ce68b483bfb60bfd3f6fc4
Thilina Ishan <151310264+ThilinaIshan@users.noreply.github.com>
2025-01-30 12:51:20 +0530

    remove the ItemList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import Layout from './components/Layout'
-import ItemList from './components/itemList/ItemList'
+import CardContainer from "./components/cardContainer/CardContainer";
+import ItemCard from "./components/itemCard/ItemCard";
+import Layout from "./components/Layout";
 
-import './styles/App.css'
+import "./styles/App.css";
 function App() {
-
   return (
     <>
       <Layout>
-      <ItemList/>
-    </Layout>
-   
-      
+        <div className="flex justify-center items-center bg-gray-50">
+          <CardContainer>
+            {Array.from({ length: 30 }).map((_, index) => (
+              <ItemCard key={index} />
+            ))}
+          </CardContainer>
+        </div>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/cardContainer/CardContainer.jsx
+++ src/components/cardContainer/CardContainer.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function CardContainer({ children }) {
   return (
     <div
-    className="w-full bg-white border-[2px] border-black/30 shadow-lg rounded-lg overflow-auto px-6 py-6 
+      className="w-full bg-white border-[2px] border-black/30 shadow-lg rounded-lg overflow-auto px-6 py-6 
     grid gap-6 mx-7"
-style={{
-gridTemplateColumns: "repeat(auto-fit, minmax(185px, 1fr))", // Ensures dynamic column resizing
-maxHeight: "60vh", // Improved height for better usability
-}}
+      style={{
+        gridTemplateColumns: "repeat(auto-fit, minmax(185px, 1fr))", // Ensures dynamic column resizing
+        maxHeight: "60vh", // Improved height for better usability
+      }}
     >
       {children}
     </div>
