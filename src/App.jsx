4ebd537760cfa8df8a347583b420827f2a333154
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-02-15 16:00:52 +0530

    create charts for reports

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "./context/AuthContext";
-import LoginPage from "./pages/LoginPage";
 import OutletPage from "./pages/OutletPage";
 import "./styles/App.css";
 import Home from "./pages/Home.jsx";
@@ -16,6 +15,7 @@ import ReturnHistoryPage from "./pages/ReturnHistory.jsx";
 
 import FactoryStaffPage from "./pages/FactoryStaffPage.jsx";
 import OwnerPage from "./pages/OwnerPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   const PrivateRoute = ({ children }) => {
@@ -27,29 +27,29 @@ function App() {
     <>
       <Router>
         <Routes>
-          {/* Show Home page */}
+       
           <Route path="/" element={<Home />} />
 
-          {/* Show login page first */}
-          <Route path="/login" element={<LoginPage />} />
+         
+          <Route path="/login" element={<LoginPage/>} />
 
-          {/* Protect other routes */}
+         
           <Route path="/outlet" element={<OutletPage />} />
           {/* <PrivateRoute><OutletPage/></PrivateRoute> */}
 
-          {/* Show order history page */}
+     
           <Route path="/order-history" element={<OrderHistoryPage />} />
 
           <Route path="/factory-Staff" element={<FactoryStaffPage/>}/>
 
           <Route path="/owner" element={<OwnerPage/>}/>
 
-          {/* Show customer order history page */}
+         
           <Route
             path="/customer-order-history"
             element={<CustomerOrderHistoryPage />}
           />
-          {/* Show customer order history page */}
+      
           <Route path="/return-history" element={<ReturnHistoryPage />} />
 
           <Route path="*" element={<Navigate to="/" />} />
