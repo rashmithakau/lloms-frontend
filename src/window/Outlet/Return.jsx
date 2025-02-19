43b73ab85ed275d3e751d21353714d5f17ca10d5
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-02-19 10:50:24 +0530

    update order and return in outlet

--- src/window/Outlet/Return.jsx
+++ src/window/Outlet/Return.jsx
@@ -6,7 +6,7 @@ import ActionContainer from "../../components/ActionContainer/ActionContainer";
 import OrderAction from "../../components/OrderAction/OrderAction";
 import ReturnAction from "../../components/ReturnAction/ReturnAction";
 import Image from "../../assets/2254.jpg_wh860.jpg";
-
+import { useState } from "react";
 
 
 function Return() {
@@ -18,17 +18,49 @@ function Return() {
     price: 100.0,
   }));
 
+  const [orderItems, setOrderItems] = useState([]);
+
+  const handleItemClick = (item) => {
+    const existingItem = orderItems.find(
+      (orderItem) => orderItem.id === item.productId
+    );
+
+    if (existingItem) {
+      setOrderItems(
+        orderItems.map((orderItem) =>
+          orderItem.id === item.productId
+            ? { ...orderItem, quantity: orderItem.quantity + 1 }
+            : orderItem
+        )
+      );
+    } else {
+      setOrderItems([
+        ...orderItems,
+        {
+          id: item.productId,
+          name: item.productName,
+          price: item.price,
+          quantity: 1,
+          discount: 0,
+        },
+      ]);
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-center items-center my-2">
         <CardContainer>
           {items.map((item, index) => (
-            <ItemCard key={index} item={item} />
+            <ItemCard 
+              key={index} 
+              item={item}
+              onClick={() => handleItemClick(item)} />
           ))}
         </CardContainer>
       </div>
       <div className="flex">
-        <OrderTable tType="return" />
+        <OrderTable tType="return" products={orderItems} setProducts={setOrderItems}/>
         <ActionContainer>
           <ReturnAction />
         </ActionContainer>
