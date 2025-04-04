dfcb126d985b189c4b16a5cf26a6387a104534b5
dihaxn <ihanlaknuka@gmail.com>
2025-04-05 00:00:02 +0530

    Make mobile view of web site

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -4,7 +4,6 @@ import Footer from "../websiteComponents/footer/Footer";
 import Product_Card from "../websiteComponents/ProductCard/Product_Card";
 import "../websiteComponents/scrollbar.css";
 import { getAllProducts } from "../api/product-service/productController";
-import ShopPopup from "../websiteComponents/Popups/ShopPopup.jsx";
 
 const ShopPage = () => {
   const [items, setItems] = useState([]);
@@ -128,15 +127,3 @@ const ShopPage = () => {
 };
 
 export default ShopPage;
-
-/*<br/>
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-          <button
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-              onClick={() => setIsPopupOpen(true)}
-          >
-            Open Popup
-          </button>
-          <ShopPopup isOpen={isPopupOpen} onClose={() => setIsPopupOpen(false)} />
-        </div>
-        */
