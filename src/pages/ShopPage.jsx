e3d7858791b58f1f430a78203f9bf301abffb273
dihaxn <ihanlaknuka@gmail.com>
2025-04-05 00:14:40 +0530

    Make mobile view of web site

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -127,3 +127,15 @@ const ShopPage = () => {
 };
 
 export default ShopPage;
+
+/*<br/>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+          <button
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              onClick={() => setIsPopupOpen(true)}
+          >
+            Open Popup
+          </button>
+          <ShopPopup isOpen={isPopupOpen} onClose={() => setIsPopupOpen(false)} />
+        </div>
+        */
